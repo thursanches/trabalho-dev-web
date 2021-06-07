6de66960b2fb2c227f83426e7e563ecd9d7ba524
Juan <juan34063@gmail.com>
2021-06-07 18:21:59 -0300

    organizando arquivos estáticos e iniciando criação dos modelos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.first_name
